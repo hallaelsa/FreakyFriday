9d212b8408db8f207d35757bc9f033e159965d51
Miina Lervik <hallalesa@hotmail.com>
2016-09-24 20:00:29 +0200

    lagt til dropdown bar til supercar 1 2 3 og satt på stil

--- add.js
+++
@@ -1,7 +0,0 @@
-var currentPage = location.href;
-var allA = document.getElementsByTagName('A');
-for(var i = 0, len = allA.length; i < len; i++) {
-    if(allA[i].href == currentPage) {
-         allA[i].className = "active";
-    }
-}
